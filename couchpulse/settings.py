00881f0c42ccdd8df2415df7022244b4adb66e61
Daniel Roberts <droberts@dimagi.com>
2013-12-30 17:32:47 -0500

    use aboslute path in settings

--- couchpulse/settings.py
+++ couchpulse/settings.py
@@ -1,6 +1,13 @@
 from ConfigParser import ConfigParser
+import os
 
 _parser = ConfigParser()
-_parser.read('alembic.ini')
+_filepath = os.path.join(
+    os.path.dirname(__file__),
+    '..',
+    'alembic.ini'
+)
+print _filepath
+_parser.read(_filepath)
 
 SQLALCHEMY_URL = _parser.get('alembic', 'sqlalchemy.url')
