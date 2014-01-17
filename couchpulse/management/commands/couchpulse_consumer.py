98d0a8fb4322c2d6d7652172ee14cd63f928cd2b
Daniel Roberts <droberts@dimagi.com>
2014-01-17 14:21:44 -0500

    modify management command to allow specifying offsets

--- couchpulse/management/commands/couchpulse_consumer.py
+++ couchpulse/management/commands/couchpulse_consumer.py
@@ -1,8 +1,14 @@
 from django.core import cache
 from django.core.management import BaseCommand
+from raven.utils import json
 from couchpulse import logreader
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        logreader.consume_forever(cache=cache.get_cache('redis'))
+        if args:
+            offsets = dict(enumerate(json.loads(args[0])))
+        else:
+            offsets = None
+
+        logreader.consume_forever(cache=cache.get_cache('redis'), offsets=offsets)
